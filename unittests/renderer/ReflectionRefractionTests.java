4e37ec30af1969088f3a24413aea60de125ecf0e
omri <omcoch@gmail.com>
2020-06-03 00:13:10 +0300

    סעיף אחרון בשלב 7 + תחילת הבונוס

--- unittests/renderer/ReflectionRefractionTests.java
+++ unittests/renderer/ReflectionRefractionTests.java
@@ -1,5 +1,6 @@
 package renderer;
 
+import geometries.Plane;
 import geometries.Polygon;
 import org.junit.Test;
 
@@ -107,27 +108,66 @@ public class ReflectionRefractionTests {
      *
      */
     @Test
-    public void houseMirrored(){
+    public void houseMirrored() {
         Scene scene = new Scene("Test scene");
-        scene.set_camera(new Camera(new Point3D(0, 0,-350), new Vector(0, 0, 1), new Vector(0, -1, 0)));
+        scene.set_camera(new Camera(new Point3D(1000, -1000, 100), new Vector(-1, 1, -0.01), new Vector(0, 0.01, 1)));
+        scene.set_distance(1000);
+        scene.set_background(new Color(java.awt.Color.cyan));
+        scene.set_ambientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+
+        scene.addGeometries( //mirror
+                new Polygon(new Color(40, 40, 40), new Material(0, 0, 0, 0, 1), //
+                        new Point3D(20, -50, 0), new Point3D(20, 50, 0), new Point3D(20, 50, 50), new Point3D(20, -50, 50)), //
+                //house
+                new Polygon(new Color(java.awt.Color.blue), new Material(0.5, 0.5, 60, 0.5, 0),//
+                        new Point3D(20, -20, 0), new Point3D(20, -20, 30), new Point3D(40, -20, 30), new Point3D(40, -20, 0)),//
+                //roof
+                new Triangle(new Color(java.awt.Color.red), new Material(0.5, 0.5, 60), //
+                        new Point3D(20, -20, 30), new Point3D(40, -20, 30), new Point3D(20, -20, 40)), //
+
+                new Sphere(new Color(java.awt.Color.red), new Material(0.2, 0.2, 30, 0.5, 0), // )
+                        15, new Point3D(40, 40, 20)), //
+                //grass
+                new Plane(new Color(java.awt.Color.green), new Point3D(1, 0, 0), new Point3D(0, 1, 0), new Point3D(1, 1, 0))
+        );
+
+        scene.addLights(new PointLight(new Color(java.awt.Color.white), new Point3D(40, 40, 20), 1.2, 0.00005, 0.0000005));
+
+        ImageWriter imageWriter = new ImageWriter("house Mirrored", 200, 200, 600, 600);
+        Render render = new Render(imageWriter, scene);
+
+        render.renderImage();
+        render.writeToImage();
+
+    }
+
+    @Test
+    public void testttt() {
+        Scene scene = new Scene("Test scene");
+        scene.set_camera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
         scene.set_distance(1000);
         scene.set_background(Color.BLACK);
         scene.set_ambientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
 
         scene.addGeometries( //
-                new Polygon(new Color(20, 20, 20), new Material(0, 0, 0,0,1), //
-                        new Point3D(10, -40, 0), new Point3D(10, 40, 0), new Point3D(10, 40, 40),new Point3D(10,-40,40)), //
-                new Polygon(new Color(java.awt.Color.blue),new Material(0.5,0.5,50,0.5,0),//
-                        new Point3D(10,-10,0),new Point3D(10,-10,20),new Point3D(20,-10,20),new Point3D(20,-10,0) ),//
-                new Triangle(new Color(java.awt.Color.red), new Material(0.5, 0.5, 60), //
-                        new Point3D(10, -10, 20), new Point3D(20, -10, 20), new Point3D(10, -10, 30)), //
-                new Sphere(new Color(java.awt.Color.yellow), new Material(0.2, 0.2, 30), // )
-                        10, new Point3D(20, 20, 10)));
+                new Triangle(Color.BLACK, new Material(0.5, 0.5, 60), //
+                        new Point3D(-150, 150, 115), new Point3D(150, 150, 135), new Point3D(75, -75, 150)), //
+                new Triangle(Color.BLACK, new Material(0.5, 0.5, 60, 0, 0.5), //
+                        new Point3D(-150, 150, 115), new Point3D(-70, -70, 140), new Point3D(75, -75, 150)), //
+                new Sphere(new Color(java.awt.Color.BLUE), new Material(0.2, 0.2, 30, 0.6, 0), // )
+                        30, new Point3D(60, -50, 50)),
+                new Sphere(new Color(java.awt.Color.red), new Material(0.2, 0.2, 30, 0.6, 0), // )
+                        30, new Point3D(-60, -50, 50))
+        );
 
         scene.addLights(new SpotLight(new Color(700, 400, 400), //
-                new Point3D(60, -50, 0), 1, 4E-5, 2E-7, new Vector(0, 0, 1)));
+                        new Point3D(60, -50, 0), 1, 0.005, 0.000005, new Vector(0, 0, 1)),
 
-        ImageWriter imageWriter = new ImageWriter("house Mirrored", 200, 200, 600, 600);
+                new PointLight(new Color(700, 400, 400), //
+                        new Point3D(-60, -20, 0), 1, 0.005, 0.00005)
+        );
+
+        ImageWriter imageWriter = new ImageWriter("testtodelete", 200, 200, 600, 600);
         Render render = new Render(imageWriter, scene);
 
         render.renderImage();
